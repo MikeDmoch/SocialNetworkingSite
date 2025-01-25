db759c19bba37245d000b2a54ba1c5247af6834c
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-25 20:37:31 +0100

    Add project files.

---
+++ SocialNetworkingSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkingSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

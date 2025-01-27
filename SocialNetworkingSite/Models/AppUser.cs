e70abd2b588549e4fb9cc470edca8e7b3500aff0
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-27 11:14:40 +0100

    added Club

--- SocialNetworkingSite/Models/AppUser.cs
+++ SocialNetworkingSite/Models/AppUser.cs
@@ -1,6 +1,11 @@
-﻿namespace SocialNetworkingSite.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SocialNetworkingSite.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
     }
 }

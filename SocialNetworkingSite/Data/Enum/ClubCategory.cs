4c6e2fe357677b1dba94f06093aed22ad13d3c28
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-01-27 11:28:23 +0100

    finished Models

---
+++ SocialNetworkingSite/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialNetworkingSite.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance   
+    };
+}
---
+++ SocialNetworkingSite/Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialNetworkingSite.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    };
+}

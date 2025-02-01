b94be3e9b2c0b81ffa5fdb88c393cc7b1abbb093
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-02-01 19:14:50 +0100

    Added Club controller and view

---
+++ SocialNetworkingSite/Views/Club/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialNetworkingSite.Views.Club
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

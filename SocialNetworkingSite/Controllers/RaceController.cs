a31e1026c380c22d0942dae159893f22a98fe032
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-02-01 19:21:25 +0100

    added race controller and view

---
+++ SocialNetworkingSite/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetworkingSite.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

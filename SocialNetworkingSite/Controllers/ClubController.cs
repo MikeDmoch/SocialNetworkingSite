aeb39f873671834f7da16aab79f121d7bbbada34
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-02-02 22:20:38 +0100

    minor changes

--- SocialNetworkingSite/Controllers/ClubController.cs
+++ SocialNetworkingSite/Controllers/ClubController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetworkingSite.Data;
 
 namespace SocialNetworkingSite.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var clubs = _context.Clubs.ToList();
             return View();
         }
     }

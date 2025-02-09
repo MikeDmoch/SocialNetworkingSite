a0b1590590065bf071fc7819f74735d05c98553e
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-02-09 23:25:35 +0100

    interface

---
+++ SocialNetworkingSite/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetworkingSite.Models;
+
+namespace SocialNetworkingSite.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<List<Race>> GetAllUserRaces();
+        Task<List<Club>> GetAllUserClubs();
+        Task<AppUser> GetUserById(string id);
+        Task<AppUser> GetByIdNoTracking(string id);
+        bool Update(AppUser user);
+        bool Save();
+    }
+}

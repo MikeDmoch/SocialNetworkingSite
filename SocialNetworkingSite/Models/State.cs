219fd044e6316d3a5140749e95f1de34ee10d82d
Michal Dmoch <m.dmoch.biz@gmail.com>
2025-02-11 20:39:43 +0100

    state

---
+++ SocialNetworkingSite/Models/State.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetworkingSite.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+    }
+}
